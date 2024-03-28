cda2ff281c8a9d125e3317d1cb182bec2ab9a6de
daniella <daniella.chacham@skahalb.org>
2024-03-28 19:30:50 -0400

    organized the different Alzherimers symptoms

--- magic.js
+++ magic.js
@@ -2,3 +2,50 @@
 function displayDate(){
     document.getElementById('date').innerHTML = Date()
 }
+
+
+
+
+
+
+
+
+
+
+
+const a_stage1 = [
+    'Remembering a name',
+    'Recalling recent events',
+'Remembering where a vauable object was placed(of significance)',
+'Making plans',
+'Basic organizational skills',
+
+
+    
+     
+];
+
+const a_stage2 = [
+    'Critical issues remembering events',
+    'Issues with learning new things',
+'Difficulty forming new mememories',
+'Trouble remembering own name however, not details about own life(phone#, address)',
+'Difficulty with reading, writing, and working with numbers',
+'Recognizing that people are familiar but forgetting names (could even include child or spouse)',
+'Lossing track of time and setting',
+'Suddenly moody or withdrawn from social settings',
+'Experiencing Hallucinations (was not present before symtoms began)',
+'Paranoia (was not present before symtoms began)',
+'Delusions (was not present before symtoms began)',
+'Restless,agitated,nervous,tearful',
+
+
+];
+
+const a_stage3 = [
+'Loss of basic physical activities(walking, sitting eating)',
+'Loss of bowel and bladder control',
+'Unable to have a converstation (can say some words and phrases)',
+
+
+];
