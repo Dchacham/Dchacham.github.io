4738957d3b220345db271885771fb4a1a9acbc56
daniella <daniella.chacham@skahalb.org>
2024-03-31 13:52:13 -0400

    organized the schizophrenia symptoms into its stages

--- magic.js
+++ magic.js
@@ -5,20 +5,12 @@ function displayDate(){
 
 
 
-
-
-
-
-
-
-
-
 const a_stage1 = [
     'Remembering a name',
     'Recalling recent events',
-'Remembering where a vauable object was placed(of significance)',
-'Making plans',
-'Basic organizational skills',
+    'Remembering where a vauable object was placed(of significance)',
+    'Making plans',
+    'Basic organizational skills',
 
 
     
@@ -28,24 +20,55 @@ const a_stage1 = [
 const a_stage2 = [
     'Critical issues remembering events',
     'Issues with learning new things',
-'Difficulty forming new mememories',
-'Trouble remembering own name however, not details about own life(phone#, address)',
-'Difficulty with reading, writing, and working with numbers',
-'Recognizing that people are familiar but forgetting names (could even include child or spouse)',
-'Lossing track of time and setting',
-'Suddenly moody or withdrawn from social settings',
-'Experiencing Hallucinations (was not present before symtoms began)',
-'Paranoia (was not present before symtoms began)',
-'Delusions (was not present before symtoms began)',
-'Restless,agitated,nervous,tearful',
+    'Difficulty forming new mememories',
+    'Trouble remembering own name however, not details about own life(phone#, address)',
+    'Difficulty with reading, writing, and working with numbers',
+    'Recognizing that people are familiar but forgetting names (could even include child or spouse)',
+    'Lossing track of time and setting',
+    'Suddenly moody or withdrawn from social settings',
+    'Experiencing Hallucinations (was not present before symtoms began)',
+    'Paranoia (was not present before symtoms began)',
+    'Delusions (was not present before symtoms began)',
+    'Restless,agitated,nervous,tearful',
 
 
 ];
 
-const a_stage3 = [
-'Loss of basic physical activities(walking, sitting eating)',
-'Loss of bowel and bladder control',
-'Unable to have a converstation (can say some words and phrases)',
+    const a_stage3 = [
+    'Loss of basic physical activities(walking, sitting eating)',
+    'Loss of bowel and bladder control',
+    'Unable to have a converstation (can say some words and phrases)',
+
+];
 
 
+
+const s_stage1 = [
+'Social isolation', 
+ 'Neglecting personal hygiene', 
+ 'Mild or poorly formed Hallucinations', 
+ 'Anxiety',
+'Sleep problems',
+'Difficulty concentrating',
+
+
+];
+
+const s_stage2 = [
+'Hallucinations (false senses such as hearing, seeing, smelling, feeling)',
+'Delusions',
+'Confusion and disorganized thoughts',
+'Exessive/useless movement',
+'Mumbling',
+'Wandering',
+'Laughing to oneself',
+'Numbness of emotions',
+];
+
+const s_stage3 = [
+'Social withdrawl',
+'Difficulty planning and participating in activities',
+'Reduced or absent facial expressions',
+'Flat, monotone voice',
+'General disinterest',
 ];
